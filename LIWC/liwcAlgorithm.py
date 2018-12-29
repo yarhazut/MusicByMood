eb9277c2a93eb62e3bc9773657dd111fc23397f3
Neta Savir <netasavir@gmail.com>
2018-12-29 19:12:33 +0200

    update

--- LIWC/liwcAlgorithm.py
+++ LIWC/liwcAlgorithm.py
@@ -1,40 +1,43 @@
 import re
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-with open('LIWC2007_English100131.dic') as f:
-    content = f.readlines()
+
+subject_To_Index = dict()
+names_toSujectIndex=dict()
 # you may also want to remove whitespace characters like `\n` at the end of each line
-content = [x.strip() for x in content]
-dicNames_toValues = dict()
-names_toSuject=dict()
-var = 'true';
-for line in content:
-    if (var=='true' and line != '%'):
-        lines=line.split("\t");
-        dicNames_toValues[lines[0]]=lines[1];
-    elif(line != '%'):
-        lines=re.split('\t|( )',line);
-        lines = [x for x in lines if x is not None]
-        myarray = np.asarray(lines)
-        lines[0]=lines[0].replace('*', '')
-        names_toSuject[lines[0]]=myarray[1:len(myarray)]
-    if (line == '%'):
-        var='false';
-gettysburg = '''Four score and seven years ago our fathers brought forth on
-  this continent a new nation, conceived in liberty, and dedicated to the
-  proposition that all men are created equal. Now we are engaged in a great
-  civil war, testing whether that nation, or any nation so conceived and so
-  dedicated, can long endure. We are met on a great battlefield of that war.
-  We have come to dedicate a portion of that field, as a final resting place
-  for those who here gave their lives that that nation might live. It is
-  altogether fitting and proper that we should do this.'''
-gettysburg_tokens = re.split('\t|( )',gettysburg);
-gettysburg_tokens = [x for x in gettysburg_tokens if x is not None]
-buckets = [0] * 464
-for word in gettysburg_tokens:
-    if (word.lower() in names_toSuject):
-        subjects=names_toSuject[word.lower()]
-        for subject in subjects:
-            buckets[int(subject)]+=1
-bucket1 = [0] * 464
-print(cosine_similarity([buckets], [buckets]))
+def open_LIWC_file():
+    liwc_content = ''
+    with open('LIWC2007_English100131.dic') as f:
+        liwc_content = f.readlines()
+        content = [x.strip() for x in liwc_content]
+    return content
+
+def map_text_to_liwc_cat(word_frec_dic,text):
+    text_length=len(text)
+    liwc_content=open_LIWC_file()
+    liwc_part_1 = 'true'
+    for line in liwc_content:
+        if (liwc_part_1=='true' and line != '%'):
+            lines = line.split("\t")
+            subject_To_Index[lines[0]] = lines[1]
+        elif(line != '%'):
+            lines = re.split('\t|( )',line)
+            lines = [x for x in lines if x is not None]
+            myarray = np.asarray(lines)
+            lines[0] = lines[0].replace('*', '')
+            names_toSujectIndex[lines[0]] = myarray[1:len(myarray)]
+        if (line == '%'):
+            liwc_part_1 = 'false'
+    buckets = {}
+    for word in word_frec_dic:
+        if (word in names_toSujectIndex):
+            subjects = names_toSujectIndex[word.lower()]
+            for subject in subjects:
+                if int(subject) in buckets:
+                    buckets[int(subject)] += word_frec_dic[word]
+                else:
+                    buckets[int(subject)] = word_frec_dic[word]
+    for subject in buckets:
+        buckets[subject] = format(buckets[subject]/text_length, '.4f')
+
+    return buckets
--- textParser.py
+++ textParser.py
@@ -1,25 +1,24 @@
+from LIWC import liwcAlgorithm
 
 def read_txt(file_name):
     with open(file_name, "r") as myfile:
         data = myfile.readlines()
         return ','.join(data)
 
-def parse_txt(text):
+def parse_txt_to_list(text):
     for char in '-.,\n':
-        data_str = data_str.replace(char, ' ')
-    data_str = data_str.lower()
+        text = text.replace(char, ' ')
+    data_str = text.lower()
     return data_str.split() #word_list
 
-def get_word_count(file_name):
+def activate_LIWC_algo(text):
     word_count_dic = {}
-    text= read_txt(file_name)
-    word_list = parse_txt(text)
+    word_list = parse_txt_to_list(text)
     for word in word_list:
         word_count_dic[word] = word_count_dic.get(word, 0) + 1
-    word_freq = []
-    for key, value in word_count_dic.items():
-        word_freq.append((value, key))
-    word_freq.sort(reverse=True)
+    return liwcAlgorithm.map_text_to_liwc_cat(word_count_dic,text)
 
+#text=read_txt('cnn_articles/helth/Chronic fatigue syndrome may be due to an overactive immune system, study finds.txt')
+#activate_LIWC_algo(text)
 
 
