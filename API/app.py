978ae40276c340d2422db2eeda6b5fcbd5d8d95b
Neta Savir <netasavir@gmail.com>
2019-04-11 15:38:23 +0300

    adding doc2vec

--- API/app.py
+++ API/app.py
@@ -7,11 +7,12 @@ from LIWC import LIWC_similarity
 from IBMWatson import watson_similarity
 from IBMWatson import IBMWatsonMain
 from googleEmbeddings.createGoogleEmbeddingsPosting import create_Google_Embeddings_AVG_Vector_For_A_Given_Text,create_google_embd_model
+from googleEmbeddings.doc2Vec import create_Doc2Vec_For_A_Given_Text
 from All_Algo import weighted_similarity
 import urllib.request
 import sys
 from bs4 import BeautifulSoup
-
+import traceback
 # played_song_dict = {}
 class ObjRequestClass:
     def on_post(self,req,resp):
@@ -45,33 +46,30 @@ def getID(Song,artist):
 def getSongSelected(tabArray):
     returnTo={}
     textFinall=''
-    for tab in tabArray:
+    for tab_url in tabArray:
         try:
-            html = requests.get(tab)
-            soup = BeautifulSoup(html.content)
-            for script in soup(["script", "style"]):
-                script.extract()  # rip it out
-            # get text
-            text = soup.get_text()
+            page = requests.get(tab_url)
+            html_bytes = page.content
+            soup = BeautifulSoup(html_bytes, 'html.parser')
+            result_set = soup.find_all('p')
+            result_set_list = list(result_set)
+            final_txt = ''
+            for tag in result_set_list:
+                txt = tag.text
+                final_txt += ' ' + txt
 
-            # break into lines and remove leading and trailing space on each
-            lines = (line.strip() for line in text.splitlines())
-            # break multi-headlines into a line each
-            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-            # drop blank lines
-            text = '\n'.join(chunk for chunk in chunks if chunk)
-            splitted = text.split()
-            text=" ".join(splitted[50:440])
-            textFinall = translate_text(text)
-            print(text)
+            textFinall = translate_text(final_txt)
+            print(textFinall)
         except Exception as e:
-            if len(tab)>1:
+            if len(tab_url)>1:
+                traceback.print_exc()
                 print (e)
             pass
     # create text vector/dict for each algorithem
     text_liwc_lic = textParser.activate_LIWC_algo(textFinall)
     ibm_dict = IBMWatsonMain.getIBMVectorFromText(textFinall)
     txt_embd_vec = create_Google_Embeddings_AVG_Vector_For_A_Given_Text(textFinall,google_embd_model)
+    txt_doc2vec_vec = create_Doc2Vec_For_A_Given_Text(textFinall)
     song_name, artist = weighted_similarity.get_most_similar_song(text_liwc_lic, ibm_dict, txt_embd_vec)
     print('song: {0}, artist: {1}'.format(song_name, artist))
     return {'Song': song_name, 'artist': artist}
